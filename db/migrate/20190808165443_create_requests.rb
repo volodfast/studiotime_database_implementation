8554851c0150ea0fa088ef8973335d18aeed5e94
Volodymyr Bystranivskyi <vova.b@milestep.io>
2019-08-08 20:19:57 +0300

    Change defaults for booking and request migrations

--- db/migrate/20190808165443_create_requests.rb
+++ db/migrate/20190808165443_create_requests.rb
@@ -7,7 +7,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
       t.time :start_time
       t.time :end_time
       t.integer :price
-      t.integer :status
+      t.integer :status, default: 0
 
       t.timestamps
     end
